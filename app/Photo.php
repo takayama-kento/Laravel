3c31efdf0f4fa1ffb4fa17486b59c12067fec957
takayama-kento <66887879+takayama-kento@users.noreply.github.com>
2020-08-27 16:53:56 +0900

    いいね機能実装

--- app/Photo.php
+++ app/Photo.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 
 class Photo extends Model
 {
@@ -72,6 +73,15 @@ class Photo extends Model
     }
 
     /**
+     * リレーションシップ - likesテーブル
+     * @return \Illuminate\Database\Elouent\BelongsToMany
+     */
+    public function likes()
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    /**
      * アクセサ - url
      * @return string
      */
@@ -80,12 +90,37 @@ class Photo extends Model
         return Storage::cloud()->url($this->attributes['filename']);
     }
 
+    /**
+     * アクセサ - likes_count
+     * @return int
+     */
+    public function getLikesCountAttribute()
+    {
+        return $this->likes->count();
+    }
+
+    /**
+     * アクセサ - liked_by_user
+     * @return boolean
+     */
+    public function getLikedByUserAttribute()
+    {
+        if (Auth::guest()) {
+            return false;
+        }
+
+        return $this->likes->contains(function ($user) {
+            return $user->id === Auth::user()->id;
+        });
+    }
+
     /** JSONに含める属性 */
     protected $appends = [
-        'url',
+        'url', 'likes_count', 'liked_by_user',
     ];
     protected $visible = [
         'id', 'owner', 'url', 'comments',
+        'likes_count', 'liked_by_user',
     ];
 
     protected $perPage = 15;
